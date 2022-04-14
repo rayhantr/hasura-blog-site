464a14f3b097c6a2145c3fa5aac9952a96d05aaf
MD Rayhan Talukder <49080349+rayhantr@users.noreply.github.com>
2022-04-14 19:31:22 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["s.gravatar.com"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/login",
+        has: [
+          {
+            type: "cookie",
+            key: "next-auth.session-token", // localhost
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+      {
+        source: "/login",
+        has: [
+          {
+            type: "cookie",
+            key: "__Secure-next-auth.session-token", // production
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+      {
+        source: "/register",
+        has: [
+          {
+            type: "cookie",
+            key: "next-auth.session-token", // localhost
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+      {
+        source: "/register",
+        has: [
+          {
+            type: "cookie",
+            key: "__Secure-next-auth.session-token", // production
+          },
+        ],
+        permanent: false,
+        destination: "/",
+      },
+    ];
+  },
+};
