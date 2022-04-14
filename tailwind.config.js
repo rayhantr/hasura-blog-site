f15fbe08a390364b45e3521c33d0d0e382289c51
MD Rayhan Talukder <trrayhan@gmail.com>
2022-04-14 21:03:59 +0600

    typo fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		fontFamily: {
-			sans: ["Outfit", ...defaultTheme.fontFamily.sans],
-			serif: [...defaultTheme.fontFamily.serif],
-			mono: [...defaultTheme.fontFamily.mono],
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
+    extend: {},
+  },
+  plugins: [],
 };
